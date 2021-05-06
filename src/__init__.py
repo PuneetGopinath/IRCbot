4d6c648f5a9e0e93d52233ab8103f1730379b3f9
Puneet Gopinath <baalkrshna@gmail.com>
2021-05-06 11:55:52 +0530

    Update __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""
+Copyright (c) 2021 BK IRCbot team and contributors
+
+Licensed under GNU Lesser General Public License v2.1
+See the LICENSE file distributed with the source code
+OR Visit https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+
+This project is a fork of https://github.com/Orderchaos/LinuxAcademy-IRC-Bot
+
+NOTE      This program is distributed in the hope that it will be useful - WITHOUT
+           ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+           FITNESS FOR A PARTICULAR PURPOSE.
+"""
