f181def023e2bb00b9f1df774b66eb0cc0f011fa
Puneet Gopinath <baalkrshna@gmail.com>
2021-05-06 11:52:17 +0530

    Update ircbot.py

--- src/ircbot.py
+++ src/ircbot.py
@@ -1,17 +1,17 @@
-###
-# Copyright (c) 2016 Linux Academy
-# Copyright (c) 2021 Puneet Gopinath
-#
-# Licensed under GNU Lesser General Public License v2.1
-# See the LICENSE file distributed with the source code
-# OR Visit https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
-#
-# This project is a fork of https://github.com/Orderchaos/LinuxAcademy-IRC-Bot
-#
-# NOTE      This program is distributed in the hope that it will be useful - WITHOUT
-#           ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-#           FITNESS FOR A PARTICULAR PURPOSE.
-###
+"""
+Copyright (c) 2016 Linux Academy
+Copyright (c) 2021 Puneet Gopinath
+
+Licensed under GNU Lesser General Public License v2.1
+See the LICENSE file distributed with the source code
+OR Visit https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+
+This project is a fork of https://github.com/Orderchaos/LinuxAcademy-IRC-Bot
+
+NOTE      This program is distributed in the hope that it will be useful - WITHOUT
+           ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+           FITNESS FOR A PARTICULAR PURPOSE.
+"""
 
 # socket for irc socket
 import socket
